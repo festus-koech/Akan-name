018c86c0790dfd7bb0bb6207269b2c84697257dd
festus-koech <festuskoech95@gmail.com>
2020-12-14 01:24:44 +0300

    (akan)

---
+++ css/script.js
@@ -0,0 +1,8 @@
+var CC, YY, MM, DD, d, dayValue;
+var dayNames =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"];
+var maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames =["Akosua","adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+function validate() {
+    var genders = document.getElementsByName("gender");
+    if( document.formGroup.year.value==!!)
